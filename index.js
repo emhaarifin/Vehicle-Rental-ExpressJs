898e777996f4355521b4bee4c89b75866634fcd3
Alan Nugraha <101muhammadarifin@gmail.com>
2021-08-16 14:45:59 +0700

    feat: first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+require('dotenv').config();
+const createError = require('http-errors');
+const cors = require('cors');
+const express = require('express');
+const logger = require('morgan');
+
+const port = process.env.DB_PORT;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization'); // If needed
+  res.header('Access-Control-Allow-Credentials', true); // If needed
+  next();
+});
+
+app.listen(port, () => {
+  console.log('server on using port', port);
+});
+
+app.use('/file', express.static('./uploads'));
+//  catch error and forward to error handler
+
+app.use('*', (req, res, next) => {
+  const error = new createError.NotFound();
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status || 500).json({
+    message: err.message || 'internal server Error',
+  });
+});
+
+module.exports = app;
---
+++ src/confiq/db.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+const mysql = require('mysql');
+// require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.log(error.message);
+  } else {
+    console.log('Succes Connect to Database');
+  }
+});
+
+module.exports = connection;
