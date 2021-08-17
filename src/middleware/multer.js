f9b7ab57c9a83aef1c8ec1bac620ac9678481e3f
Alan Nugraha <101muhammadarifin@gmail.com>
2021-08-17 13:12:39 +0700

    update for multiple upload

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,21 +1,29 @@
 /* eslint-disable no-undef */
 const multer = require('multer');
-const maxSize = 1 * 1024 * 1024; // for 1MB
+const maxSize = 25 * 1024 * 1024; // for 1MB
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './uploads');
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
+    cb(null, Date.now() + '-' + file.originalname.replace(/ /g, ''));
   },
 });
+
 const upload = multer({
   storage: storage,
   fileFilter: (req, file, cb) => {
-    if (file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg') {
+    if (
+      file.mimetype == 'image/png' ||
+      file.mimetype == 'image/jpg' ||
+      file.mimetype == 'image/jpeg' ||
+      file.mimetype == 'image/svg' ||
+      file.mimetype == 'image/svg+xml'
+    ) {
       cb(null, true);
     } else {
       cb(null, false);
+      console.log(file);
       return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
     }
   },
